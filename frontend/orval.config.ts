972fbfb4f3b70254623849b45f7c2f04d2a06556
Joseph Clark <josephc@slalom.com>
2025-10-07 11:52:31 -0700

    Adding baseline for orval

---
+++ frontend/orval.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'orval';
+
+export default defineConfig({
+  caja: {
+    input: '../openapi.json',
+    output: {
+      target: './src/api/generated.ts',
+      client: 'react-query',
+      mock: true
+    },
+  }
+});
