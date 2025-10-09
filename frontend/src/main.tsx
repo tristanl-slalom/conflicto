41fd532c8f3683a2b1e12592de1f24a8e86bc66f
Joseph Clark <josephc@slalom.com>
2025-10-08 18:00:53 -0700

    Fixing base url

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -3,7 +3,7 @@ import { RouterProvider } from '@tanstack/react-router'
 import { getRouter } from './router'
 import axios from 'axios'
 
-axios.defaults.baseURL = 'http://localhost:8000';
+axios.defaults.baseURL = import.meta.env.VITE_API_BASE_URL || 'http://localhost:8000';
 
 // Create the router instance
 const router = getRouter()
